b6a2ddeaebfdd8ece7e11ca7447eb774763e3747
Chaiwat Sombatthepsut <devleveling199x@gmail.com>
2018-11-17 16:07:31 +0700

    Initial Commit

---
+++ FirebaseSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSample
+//
+//  Created by mercury on 17/11/2561 BE.
+//  Copyright © 2561 mercury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
